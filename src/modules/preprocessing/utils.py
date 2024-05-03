8786974c33dd5062f77a74a96097acd51c286f3a
PΣTR PΣTRUK <47356327+as1mple@users.noreply.github.com>
2024-05-03 14:15:25 +0300

    Update utils.py

--- src/modules/preprocessing/utils.py
+++ src/modules/preprocessing/utils.py
@@ -1,22 +1,26 @@
 import re
 import string
 from typing import List
-import unidecode
+from unidecode import unidecode
 
 from nltk.corpus import stopwords, twitter_samples
 
 
 def preprocessing(text: str) -> str:
-    lowercase_text = text.lower()
-    text_without_diacritics = unidecode.unidecode(lowercase_text)
-    text_without_brackets = re.sub(r"\[.*?\]", "", text_without_diacritics)
-    text_without_urls = re.sub(r"https?://\S+|www\.\S+", "", text_without_brackets)
-    text_without_tags = re.sub(r"<.*?>+", "", text_without_urls)
-    text_without_punctuation = re.sub(r"[{}]".format(re.escape(string.punctuation)), "", text_without_tags)
-    text_without_newlines = re.sub(r"\n", "", text_without_punctuation)
-    text_without_digits = re.sub(r"\w*\d\w*", "", text_without_newlines)
-
-    return text_without_digits
+    regex_patterns = {
+        "Remove content inside brackets": r"\[.*?\]",
+        "Remove URLs": r"https?://\S+|www\.\S+",
+        "Remove HTML tags": r"<.*?>+",
+        "Remove punctuation": r"[{}]".format(re.escape(string.punctuation)),
+        "Remove newlines": r"\n",
+        "Remove digits": r"\w*\d\w*"
+    }
+    
+    for description, pattern in regex_patterns.items():
+        text = re.sub(pattern, "", text)
+    
+    text = unidecode(text.lower())
+    return text
 
 
 def remove_stopwords(text: List[str]) -> List[str]:
